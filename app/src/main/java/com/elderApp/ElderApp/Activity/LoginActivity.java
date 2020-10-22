8a57f0f352d0ef908f86714038a25058579df8da
beta0221 <beta0221@gmail.com>
2020-10-23 00:38:52 +0800

    login 登入api 重寫

--- app/src/main/java/com/elderApp/ElderApp/Activity/LoginActivity.java
+++ app/src/main/java/com/elderApp/ElderApp/Activity/LoginActivity.java
@@ -21,6 +21,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.elderApp.ElderApp.AlertDialog.ios_style_alert_dialog_1;
 import com.elderApp.ElderApp.Helper_Class.MySingleton;
+import com.elderApp.ElderApp.Helper_Class.apiService;
 import com.elderApp.ElderApp.Model_Class.User;
 import com.elderApp.ElderApp.R;
 import com.elderApp.ElderApp.Helper_Class.myJsonRequest;
@@ -74,7 +75,8 @@ public class LoginActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.button1:
-                    login_request();
+                    //login_request();
+                    loginRequest();
                     break;
                 case R.id.button2:
                     Intent intent = new Intent();
@@ -87,74 +89,144 @@ public class LoginActivity extends AppCompatActivity {
     };
     //---------------------回報Listener------------------------------------------------------------
 
-    private void login_request() {
-        String url = "https://www.happybi.com.tw/api/auth/login";
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("email", account.getText().toString());
-            jsonObject.put("password", password.getText().toString());
-            jsonObject.put("androidVer", versionCode);
-        } catch (JSONException e) {
-        }
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(1, url, jsonObject,
-                new Response.Listener<JSONObject>() {
+    private void loginRequest(){
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            if (response.has("access_token")) {
-                                preference.edit().putString("access_token", response.getString("access_token")).commit();
-                                preference.edit().putString("email",account.getText().toString()).commit();
-                                preference.edit().putString("password",password.getText().toString()).commit();
-                                user.access_token = response.getString("access_token");
-                                user.user_id = response.getInt("user_id");
-                                user.id_code = response.getString("id_code");
-                                user.email = response.getString("email");
-                                user.name = response.getString("name");
-                                user.wallet = response.getInt("wallet");
-                                user.rank = response.getInt("rank");
-//                                user.org_rank=response.getInt("org_rank");
-                                upload_push_token();
-                            } else if (response.has("android_update_url")) {
-                                update_url = response.getString("android_update_url");
-                                new ios_style_alert_dialog_1.Builder(context).setTitle("已有更新版本").setMessage("請更新後重新登入").setPositiveButton("是", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        Intent i = new Intent(Intent.ACTION_VIEW);
-                                        i.setData(Uri.parse(update_url));
-                                        context.startActivity(i);
-                                    }
-                                }).setNegativeButton("否", null).show();
-                            }
-                        } catch (JSONException e) {
-                        }
+        final String _email = account.getText().toString();
+        final String _password = password.getText().toString();
+
+        apiService.loginRequest(this.context, _email, _password, this.versionCode, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                if (response.has("android_update_url")) {
+                    try {
+                        String androidUpdateUrl =  response.getString("android_update_url");
+                        askToUpdate(androidUpdateUrl);
+                    } catch (JSONException e) {
                     }
-                }, new Response.ErrorListener() {
+                    return;
+                }
+
+                if (response.has("access_token")) {
+
+                    try {
+                        preference.edit().putString("access_token", response.getString("access_token")).commit();
+                        preference.edit().putString("email",_email).commit();
+                        preference.edit().putString("password",_password).commit();
+
+                        user.access_token = response.getString("access_token");
+                        user.user_id = response.getInt("user_id");
+                        user.id_code = response.getString("id_code");
+                        user.email = response.getString("email");
+                        user.name = response.getString("name");
+                        user.wallet = response.getInt("wallet");
+                        user.rank = response.getInt("rank");
+                    }catch(JSONException e){
+                    }
+                    upload_push_token();
+                }
+
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                System.out.println(error.toString());
                 if (error.networkResponse != null) {
-                    new ios_style_alert_dialog_1.Builder(context)
-                            .setTitle("登入失敗")
-                            .setMessage("帳號或密碼錯誤")
-                            .show();
-                } else {
-                    new ios_style_alert_dialog_1.Builder(context)
-                            .setTitle("登入失敗")
-                            .setMessage("請檢查網路連線")
-                            .show();
+                    new ios_style_alert_dialog_1.Builder(context).setTitle("登入失敗").setMessage("帳號或密碼錯誤").show();
+                    return;
                 }
+                new ios_style_alert_dialog_1.Builder(context).setTitle("登入失敗").setMessage("請檢查網路連線").show();
             }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/x-www-form-urlencoded");
-                headers.put("Content-Type", "application/json");
-                return headers;
-            }
-        };
-        MySingleton.getInstance(context).getRequestQueue().add(jsonObjectRequest);
+        });
     }
+
+
+    /**
+     * 詢問是否更新版本
+     * @param updateUrl
+     */
+    private void askToUpdate(final String updateUrl){
+
+        new ios_style_alert_dialog_1.Builder(context).setTitle("已有更新版本").setMessage("請更新後重新登入").setPositiveButton("是", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(updateUrl));
+                context.startActivity(i);
+            }
+        }).setNegativeButton("否", null).show();
+
+    }
+
+
+//    private void login_request() {
+//        String url = "https://www.happybi.com.tw/api/auth/login";
+//        JSONObject jsonObject = new JSONObject();
+//        try {
+//            jsonObject.put("email", account.getText().toString());
+//            jsonObject.put("password", password.getText().toString());
+//            jsonObject.put("androidVer", versionCode);
+//        } catch (JSONException e) {
+//        }
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(1, url, jsonObject,
+//                new Response.Listener<JSONObject>() {
+//
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        try {
+//                            if (response.has("access_token")) {
+//                                preference.edit().putString("access_token", response.getString("access_token")).commit();
+//                                preference.edit().putString("email",account.getText().toString()).commit();
+//                                preference.edit().putString("password",password.getText().toString()).commit();
+//                                user.access_token = response.getString("access_token");
+//                                user.user_id = response.getInt("user_id");
+//                                user.id_code = response.getString("id_code");
+//                                user.email = response.getString("email");
+//                                user.name = response.getString("name");
+//                                user.wallet = response.getInt("wallet");
+//                                user.rank = response.getInt("rank");
+////                                user.org_rank=response.getInt("org_rank");
+//                                upload_push_token();
+//                            } else if (response.has("android_update_url")) {
+//                                update_url = response.getString("android_update_url");
+//                                new ios_style_alert_dialog_1.Builder(context).setTitle("已有更新版本").setMessage("請更新後重新登入").setPositiveButton("是", new DialogInterface.OnClickListener() {
+//                                    @Override
+//                                    public void onClick(DialogInterface dialog, int which) {
+//                                        Intent i = new Intent(Intent.ACTION_VIEW);
+//                                        i.setData(Uri.parse(update_url));
+//                                        context.startActivity(i);
+//                                    }
+//                                }).setNegativeButton("否", null).show();
+//                            }
+//                        } catch (JSONException e) {
+//                        }
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                if (error.networkResponse != null) {
+//                    new ios_style_alert_dialog_1.Builder(context)
+//                            .setTitle("登入失敗")
+//                            .setMessage("帳號或密碼錯誤")
+//                            .show();
+//                } else {
+//                    new ios_style_alert_dialog_1.Builder(context)
+//                            .setTitle("登入失敗")
+//                            .setMessage("請檢查網路連線")
+//                            .show();
+//                }
+//            }
+//        }) {
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> headers = new HashMap<String, String>();
+//                headers.put("Content-Type", "application/x-www-form-urlencoded");
+//                headers.put("Content-Type", "application/json");
+//                return headers;
+//            }
+//        };
+//        MySingleton.getInstance(context).getRequestQueue().add(jsonObjectRequest);
+//    }
 
     private void upload_push_token() {
         String url2 = "https://www.happybi.com.tw/api/auth/set_pushtoken";
--- app/src/main/java/com/elderApp/ElderApp/Helper_Class/apiService.java
+++ app/src/main/java/com/elderApp/ElderApp/Helper_Class/apiService.java
@@ -8,20 +8,60 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class apiService {
 
     private static String host = "https://www.happybi.com.tw";
 
+    /**
+     * 登入
+     * @param context
+     * @param email
+     * @param password
+     * @param versionCode
+     * @param responseListener
+     * @param errorListener
+     */
+    public static void loginRequest(Context context,String email,String password,int versionCode, Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener){
+        System.out.println("loginRequest");
+        String requestUrl = host.concat("/api/auth/login");
+        JSONObject postData = new JSONObject();
+        try{
+            postData.put("email", email);
+            postData.put("password", password);
+            postData.put("androidVer", versionCode);
+        }catch (JSONException e){
+            return;
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,requestUrl,postData,responseListener,errorListener){
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                params.put("Content-Type", "application/json");
+                return params;
+            }
+        };
+        MySingleton.getInstance(context).getRequestQueue().add(request);
+    }
+
+    /**
+     * 取得所有可選擇的組織
+     * @param context
+     * @param responseListener
+     * @param errorListener
+     */
     public static void getAssociationRequest(Context context, Response.Listener<JSONArray> responseListener, Response.ErrorListener errorListener){
         System.out.println("getAssociationRequest");
         String requestUrl = host.concat("/api/getAllAssociation");
-        System.out.println(requestUrl);
-        //new JsonArrayRequest(Re)
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,requestUrl,null,responseListener,errorListener);
-        MySingleton.getInstance(context).getRequestQueue().add(jsonArrayRequest);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,requestUrl,null,responseListener,errorListener);
+        MySingleton.getInstance(context).getRequestQueue().add(request);
     }
 
 }
